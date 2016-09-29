237eb35ee17156ca80cc366b2f59586e120e86e2
Vertig00 <kluchus@gmail.com>
2016-09-29 21:43:42 +0200

    Dodanie podstawowego generatora

--- Quicksort/src/main/java/main/Main.java
+++ Quicksort/src/main/java/main/Main.java
@@ -1,12 +1,26 @@
 package main;
 
+import java.util.Scanner;
+
 /**
  * Created by lukasz on 27.09.16.
  */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        System.out.println("podaj liczbe");
+        Generator generator = new Generator(insertNumber());
+
+    }
+
+
+    public static double insertNumber(){
+        double number = 0;
+        Scanner odczyt = new Scanner(System.in);
+
+        number = odczyt.nextInt();
+        return number;
     }
 
 }
