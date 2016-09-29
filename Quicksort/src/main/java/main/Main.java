e6ed8813de4dbdd0d13582b1f39c5da250801023
Nex0Zero <180550@edu.p.lodz.pl>
2016-09-29 22:28:01 +0200

    czas

--- Quicksort/src/main/java/main/Main.java
+++ Quicksort/src/main/java/main/Main.java
@@ -11,21 +11,28 @@ public class Main {
             10, 32, 2.523, -1.123, 0, 12.345, -9.876};
 
     public static void main(String[] args) {
-
+        mainN();
     }
+
     public static void mainN() {
         soutTab(tab, "Before quicksort");
 
+        long startTime = System.nanoTime();
         QuickSort.quicksSortCalc(tab, 0, tab.length - 1);
+        long endTime = System.nanoTime();
+
+        long duration = (endTime - startTime);
+        System.out.println("Time: " + duration + "\n");
 
         soutTab(tab, "After quicksort");
     }
+
     public static void mainK() {
         System.out.println("podaj liczbe");
         Generator generator = new Generator(insertNumber());
     }
 
-    public static double insertNumber(){
+    public static double insertNumber() {
         double number = 0;
         Scanner odczyt = new Scanner(System.in);
 
