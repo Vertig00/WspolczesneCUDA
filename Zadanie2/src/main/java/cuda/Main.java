b739bbdc06b5e340d94203a5c295a067c6cde229
Vertig0 <kluchus@gmail.com>
2016-10-31 13:11:31 +0100

    Szkielet zadania 2

---
+++ Zadanie2/src/main/java/cuda/Main.java
@@ -0,0 +1,13 @@
+package cuda;
+
+/**
+ * Created by vertig0 on 31.10.16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Zadanie 2");
+    }
+
+
+}
